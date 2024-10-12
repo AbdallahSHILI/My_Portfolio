094f2e64c7a61fced54bdf210faba11279f55600
Abdallah <shiliabdallah1234@gmail.com>
2024-10-12 13:13:20 +0200

    about footer and copy right components work successfully

--- src/Pages/About/about.jsx
+++ src/Pages/About/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css"; // Create a CSS file for styling
 import myPic from "../../Components/Assests/My-pic.jpg";
-import { AboutFooter, LevelUp } from "..";
+import { AboutFooter, CopyRight, LevelUp, MyStory } from "../index";
 
 const About = () => {
   return (
@@ -13,7 +13,7 @@ const About = () => {
       </section>
 
       <section className="content-section">
-        <img src={myPic} alt="Abdallah Shili" className="profile-pic left" />
+        <img src={myPic} alt="Abdallah Shili" className="profile_pic_aboutMe" />
         <div className="text-content">
           <p>
             As a dedicated web developer, I specialize in building robust and
@@ -29,37 +29,10 @@ const About = () => {
         </div>
       </section>
 
-      <section className="content-section reverse">
-        <div className="text-content">
-          <h2>My Story</h2>
-          <p>
-            I've always loved technology. From a young age, technology has
-            interested me. Most children play video games of some kind, from
-            Tetris to Spyro, Crash Bandicoot to Final Fantasy, I loved playing
-            with computers, but my interest didn't stop just at entertainment.
-            <br />
-            The more I played, the more I wanted to understand how things
-            worked. If something broke, I would take it apart and figure out
-            what had gone wrong, trying to get it back to normal.
-            <br />
-            I remember taking apart my computer when I was 12, an event which
-            horrified my parents.
-            <br />
-            I still build my own computers.
-            <br />
-            Now I focus on people and learning how they interact with websites,
-            making sure they can achieve their goals leaving them satisfied and
-            more likely to return as customers.
-          </p>
-        </div>
-        <img
-          src="/path-to-your-image-2.jpg"
-          alt="Abdallah Shili"
-          className="profile-pic right"
-        />
-      </section>
+      <MyStory />
       <LevelUp />
       <AboutFooter />
+      <CopyRight />
     </div>
   );
 };
