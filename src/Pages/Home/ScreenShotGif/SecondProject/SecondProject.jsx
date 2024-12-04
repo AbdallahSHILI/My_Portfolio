849371df4304c92aa75f409cb9e6fc8bb71bde34
Abdallah <shiliabdallah1234@gmail.com>
2024-12-04 10:51:04 +0100

    Refactor project components to use shared styles and assets

--- src/Pages/Home/ScreenShotGif/SecondProject/SecondProject.jsx
+++ src/Pages/Home/ScreenShotGif/SecondProject/SecondProject.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import styles from "./SecondProject.module.css";
+import styles from "../MainStyleProject.module.css";
+import gifImageMacIphone from "../../../../Components/Assests/Blank diagram.png";
+import gifImageIphone from "../../../../Components/Assests/Firefly 20231202140733.png";
 
 const SecondProject = () => {
-  return <div>SecondProject</div>;
+  return (
+    <div className={styles.gif_container}>
+      <img
+        src={gifImageMacIphone}
+        alt="Screenshot GIF"
+        className={styles.full_page_gif}
+      />
+      <img
+        src={gifImageIphone}
+        alt="Screenshot GIF"
+        className={styles.full_page_gif_max_width}
+      />
+    </div>
+  );
 };
 
 export default SecondProject;
--- src/Pages/Home/ScreenShotGif/screenshotGif.jsx
+++ src/Pages/Home/ScreenShotGif/screenshotGif.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import "./screenshotGif.css";
-import gifImageMacIphone from "../../../Components/Assests/Portfolio_ScreenShot.gif";
-import gifImageIphone from "../../../Components/Assests/Max-width_Portfolio_ScreenShot.gif";
 
 const ScreenshotGif = () => {
   return (
-    <div className="gif-container">
-      <img
-        src={gifImageMacIphone}
-        alt="Screenshot GIF"
-        className="full-page-gif"
-      />
-      <img
-        src={gifImageIphone}
-        alt="Screenshot GIF"
-        className="full-page-gif_max-width"
-      />
-    </div>
+    <>
+      <FirstProject />
+      <SecondProject />
+    </>
   );
 };
 
