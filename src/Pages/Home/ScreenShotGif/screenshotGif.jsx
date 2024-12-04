ad9afbbf81b33b68a32d6a5e3d3cc640ab13eefd
Abdallah <shiliabdallah1234@gmail.com>
2024-12-04 11:31:38 +0100

    Add project click handling and dynamic GIF rendering

--- src/Pages/Home/ScreenShotGif/screenshotGif.jsx
+++ src/Pages/Home/ScreenShotGif/screenshotGif.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./screenshotGif.css";
+import FirstProject from "./FirstProject/FirstProject";
+import SecondProject from "./SecondProject/SecondProject";
+import ThirdProject from "./ThirdProject/ThirdProject";
 
-const ScreenshotGif = () => {
+const ScreenshotGif = ({ activeProject }) => {
   return (
     <>
-      {/* <FirstProject />
-      <SecondProject /> */}
+      {/* condition to render the correct gif based on the active project 
+       clothing, tasty, chat are set in the projectList component*/}
+      {activeProject === "clothing" && <FirstProject />}
+      {activeProject === "tasty" && <SecondProject />}
+      {activeProject === "chat" && <ThirdProject />}
     </>
   );
 };
--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
 import {
   AboutFooter,
@@ -10,12 +10,21 @@ import {
   HomeServices,
   ProjectList,
 } from "../index";
+
 const Home = () => {
+  //for the maage of project so the first project is active when the page is loaded
+  const [activeProject, setActiveProject] = useState("clothing");
+
+  //function to handle the project click so the gif will change when the project is clicked
+  const handleProjectClick = (projectName) => {
+    setActiveProject(projectName);
+  };
+
   return (
     <div className="home_container">
       <MainHome />
-      <ScreenshotGif />
-      <ProjectList />
+      <ScreenshotGif activeProject={activeProject} />
+      <ProjectList onProjectClick={handleProjectClick} />
       <HomeServices />
       <LetsAddBook />
       <LevelUp />
