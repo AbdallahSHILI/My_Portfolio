0db64ddff793c36cd9a4d5c8b5818e24178514e4
Abdallah <shiliabdallah1234@gmail.com>
2024-12-04 10:29:04 +0100

    add first, second, third Projects component empty

---
+++ src/Pages/Home/ScreenShotGif/FirstProject/FirstProject.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./FirstProject.module.css";
+
+const FirstProject = () => {
+  return <div>FirstProject</div>;
+};
+
+export default FirstProject;
---
+++ src/Pages/Home/ScreenShotGif/SecondProject/SecondProject.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./SecondProject.module.css";
+
+const SecondProject = () => {
+  return <div>SecondProject</div>;
+};
+
+export default SecondProject;
